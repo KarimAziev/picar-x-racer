c78c0a6c93fdfc4cf36db8197d500b031ad8351a
KarimAziev <karim.aziiev@gmail.com>
2024-08-22 21:44:15 +0300

    perf: Optimize video streaming with async encoding and resizing

--- backend/util/video_utils.py
+++ backend/util/video_utils.py
@@ -3,57 +3,55 @@ import numpy as np
 from time import sleep
 from typing import Generator
 from util.platform_adapters import Vilib
+from concurrent.futures import ThreadPoolExecutor
 
+# Constants for target width and height of video streams
 TARGET_WIDTH = 1280
 TARGET_HEIGHT = 720
+
+# Thread pool executor for handling asynchronous tasks
+executor = ThreadPoolExecutor(max_workers=4)
 
 
 def convert_listproxy_to_array(listproxy_obj):
     """
-    Convert a ListProxy object to a numpy array with the shape (480, 640, 3).
+    Convert a ListProxy object to a NumPy array.
 
     Parameters:
-    listproxy_obj: ListProxy
-        The ListProxy object to be converted.
+        listproxy_obj (ListProxy): The ListProxy object to convert.
 
     Returns:
-    np.ndarray
-        A numpy array representation of the ListProxy object.
+        np.ndarray: The converted NumPy array.
     """
     return np.array(listproxy_obj, dtype=np.uint8).reshape((480, 640, 3))
 
 
 def enhance_frame(frame: np.ndarray) -> np.ndarray:
     """
-    Enhance the input video frame by denoising and adjusting its contrast.
+    Enhance the input video frame by denoising and applying CLAHE.
 
     Parameters:
-    frame: np.ndarray
-        The input video frame in BGR format.
+        frame (np.ndarray): The input frame to enhance.
 
     Returns:
-    np.ndarray
-        The enhanced video frame.
+        np.ndarray: The enhanced frame.
     """
     denoised_frame = cv2.fastNlMeansDenoisingColored(frame, None, 10, 10, 7, 21)
-
     lab = cv2.cvtColor(denoised_frame, cv2.COLOR_BGR2LAB)
     l, a, b = cv2.split(lab)
     clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
     cl = clahe.apply(l)
     limg = cv2.merge((cl, a, b))
     final_frame = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-
     return final_frame
 
 
 def get_frame() -> bytes:
     """
-    Get the current video frame encoded as a JPEG byte array.
+    Get the current frame as a JPEG-encoded byte array.
 
     Returns:
-    bytes
-        The JPEG encoded byte array of the current video frame.
+        bytes: The encoded frame.
     """
     frame_array = convert_listproxy_to_array(Vilib.flask_img)
     _, buffer = cv2.imencode(".jpg", frame_array)
@@ -62,11 +60,10 @@ def get_frame() -> bytes:
 
 def get_qrcode_pictrue() -> bytes:
     """
-    Get the current QR code picture frame encoded as a JPEG byte array.
+    Get the current QR code picture as a JPEG-encoded byte array.
 
     Returns:
-    bytes
-        The JPEG encoded byte array of the current QR code picture frame.
+        bytes: The encoded QR code picture.
     """
     frame_array = convert_listproxy_to_array(Vilib.flask_img)
     _, buffer = cv2.imencode(".jpg", frame_array)
@@ -75,11 +72,10 @@ def get_qrcode_pictrue() -> bytes:
 
 def get_png_frame() -> bytes:
     """
-    Get the current video frame encoded as a PNG byte array.
+    Get the current frame as a PNG-encoded byte array.
 
     Returns:
-    bytes
-        The PNG encoded byte array of the current video frame.
+        bytes: The encoded frame.
     """
     frame_array = convert_listproxy_to_array(Vilib.flask_img)
     _, buffer = cv2.imencode(".png", frame_array)
@@ -88,96 +84,87 @@ def get_png_frame() -> bytes:
 
 def get_qrcode() -> bytes:
     """
-    Get the current QR code image once it is available.
+    Get the encoded QR code image as a byte array. Waits until the QR code
+    image is available.
 
     Returns:
-    bytes
-        The byte array of the encoded QR code image.
+        bytes: The QR code image in encoded bytes.
     """
     while Vilib.qrcode_img_encode is None:
         sleep(0.2)
-
     return Vilib.qrcode_img_encode
 
 
-def default_gen():
-    """Video streaming generator function."""
-    while True:
-        # start_time = time.time()
-        frame = get_frame()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
-        sleep(0.03)
-        # end_time = time.time() - start_time
-        # print('flask fps:%s'%int(1/end_time))
-
-
-def generate_video_stream() -> Generator[bytes, None, None]:
+def resize_and_encode(frame, width, height, format=".jpg", quality=90):
     """
-    Video streaming generator function that yields JPEG encoded frames.
+    Resize and encode frame.
+
+    Parameters:
+        frame (np.ndarray): The input frame to resize and encode.
+        width (int): The target width of the frame.
+        height (int): The target height of the frame.
+        format (str): The encoding format, e.g., '.jpg' or '.png'. Default is '.jpg'.
+        quality (int): The encoding quality (only applies to JPEG). Default is 90.
+
+    Returns:
+        bytes: The encoded frame.
+    """
+    resized_frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_CUBIC)
+    encode_params = [cv2.IMWRITE_JPEG_QUALITY, quality] if format == ".jpg" else []
+    _, buffer = cv2.imencode(format, resized_frame, encode_params)
+    return buffer.tobytes()
+
+
+def async_generate_video_stream(width: int, height: int, format=".jpg", quality=90):
+    """
+    Asynchronously generate a video stream encoded in the specified format and quality.
+
+    Parameters:
+        width (int): Target width for the encoded frames.
+        height (int): Target height for the encoded frames.
+        format (str): The encoding format ('jpg' or 'png'). Default is '.jpg'.
+        quality (int): The encoding quality (applicable only for JPEG). Default is 90.
 
     Yields:
-    Generator[bytes, None, None]
-        A generator that yields a sequence of JPEG encoded frames.
+        bytes: Encoded frames in byte-string format ready for streaming.
     """
     while True:
-        frame = get_frame()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
-        sleep(0.03)
+        frame = convert_listproxy_to_array(Vilib.flask_img)
+        future = executor.submit(
+            resize_and_encode, frame, width, height, format, quality
+        )
+        encoded_frame = future.result()
+        yield (
+            b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + encoded_frame + b"\r\n"
+        )
+        sleep(0.05)
 
 
 def generate_high_quality_stream() -> Generator[bytes, None, None]:
     """
-    High quality video streaming generator function that yields enhanced, resized, and JPEG encoded frames.
+    Generate a high-quality video stream.
 
-    Yields:
-    Generator[bytes, None, None]
-        A generator that yields a sequence of high quality JPEG encoded frames.
+    Returns:
+        Generator[bytes, None, None]: A generator yielding high-quality video frames.
     """
-    while True:
-        frame_array = convert_listproxy_to_array(Vilib.flask_img)
-        enhanced_frame = enhance_frame(frame_array)
-        resized_frame = cv2.resize(
-            enhanced_frame, (TARGET_WIDTH, TARGET_HEIGHT), interpolation=cv2.INTER_CUBIC
-        )
-        _, buffer = cv2.imencode(".jpg", resized_frame)
-        frame = buffer.tobytes()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
-        sleep(0.03)
+    return async_generate_video_stream(TARGET_WIDTH, TARGET_HEIGHT)
 
 
 def generate_medium_quality_stream() -> Generator[bytes, None, None]:
     """
-    Medium quality video streaming generator function that yields resized and JPEG encoded frames.
+    Generate a medium-quality video stream.
 
-    Yields:
-    Generator[bytes, None, None]
-        A generator that yields a sequence of medium quality JPEG encoded frames.
+    Returns:
+        Generator[bytes, None, None]: A generator yielding medium-quality video frames.
     """
-    while True:
-        frame_array = convert_listproxy_to_array(Vilib.flask_img)
-        resized_frame = cv2.resize(
-            frame_array, (TARGET_WIDTH, TARGET_HEIGHT), interpolation=cv2.INTER_CUBIC
-        )
-        _, buffer = cv2.imencode(".jpg", resized_frame)
-        frame = buffer.tobytes()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
-        sleep(0.03)
+    return async_generate_video_stream(TARGET_WIDTH, TARGET_HEIGHT, quality=75)
 
 
 def generate_low_quality_stream() -> Generator[bytes, None, None]:
     """
-    Low quality video streaming generator function that yields resized and PNG encoded frames.
+    Generate a low-quality video stream.
 
-    Yields:
-    Generator[bytes, None, None]
-        A generator that yields a sequence of low quality PNG encoded frames.
+    Returns:
+        Generator[bytes, None, None]: A generator yielding low-quality video frames.
     """
-    while True:
-        frame_array = convert_listproxy_to_array(Vilib.flask_img)
-        resized_frame = cv2.resize(
-            frame_array, (640, 360), interpolation=cv2.INTER_LINEAR
-        )
-        _, buffer = cv2.imencode(".png", resized_frame)
-        frame = buffer.tobytes()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
-        sleep(0.03)
+    return async_generate_video_stream(640, 360, quality=50)
