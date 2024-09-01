1b784e4633520b9f02313e59d0b81bfb54bb29d3
KarimAziev <karim.aziiev@gmail.com>
2024-09-01 14:59:13 +0300

    refactor: replace print statements with logger in AudioHandler

--- backend/app/controllers/audio_handler.py
+++ backend/app/controllers/audio_handler.py
@@ -1,4 +1,5 @@
 from app.robot_hat.music import Music
+from app.util.logger import Logger
 import time
 from os import path
 from app.util.singleton_meta import SingletonMeta
@@ -15,6 +16,7 @@ except ImportError:
 class AudioHandler(metaclass=SingletonMeta):
     def __init__(self):
         self.music = Music()
+        self.logger = Logger("AudioController")
         self.google_speech_available = google_speech_available
         self.music.music_set_volume(100)
         self.sound_playing = False
@@ -23,27 +25,25 @@ class AudioHandler(metaclass=SingletonMeta):
     def text_to_speech(self, words: str, lang="en"):
         if google_speech_available:
             try:
-                print(f"Playing text-to-speech for: {words}")
+                self.logger.info(f"text-to-speech: {words}")
                 speech = Speech(words, lang)
                 speech.play()
             except Exception as e:
-                print(f"Error playing text-to-speech audio: {e}")
+                self.logger.error(f"Error playing text-to-speech audio: {e}")
         else:
-            print(
-                f"google_speech not available. Unable to play text-to-speech for: {words}"
-            )
+            self.logger.warning("google_speech is not available")
 
     def play_music(self, track_path: str):
         if path.exists(track_path):
             if self.is_music_playing():
-                print(f"Stopping currently playing music")
+                self.logger.info(f"Stopping currently playing music")
                 self.stop_music()
             else:
-                print(f"Playing music {track_path}")
+                self.logger.info(f"Playing music {track_path}")
                 self.music.music_play(track_path)
         else:
             text = f"The music file {track_path} is missing."
-            print(text)
+            self.logger.info(text)
 
     def stop_music(self):
         self.music.music_stop()
@@ -54,17 +54,17 @@ class AudioHandler(metaclass=SingletonMeta):
     def play_sound(self, sound_path: str):
         if path.exists(sound_path):
             if self.is_sound_playing():
-                print(f"Stopping currently playing sound")
+                self.logger.info(f"Stopping currently playing sound")
                 self.stop_sound()
             else:
-                print(f"Playing sound {sound_path}")
+                self.logger.info(f"Playing sound {sound_path}")
                 sound_length = self.music.sound_length(sound_path)
                 self.music.sound_play_threading(sound_path)
                 self.sound_playing = True
                 self.sound_end_time = time.time() + sound_length
         else:
             text = f"The sound file {sound_path} is missing."
-            print(text)
+            self.logger.info(text)
 
     def is_sound_playing(self):
         if (
