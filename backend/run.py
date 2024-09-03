272ece96eeb773fd2d1cb551b9441024ba698b03
KarimAziev <karim.aziiev@gmail.com>
2024-09-03 14:19:38 +0300

    refactor: simplify run.py and enhance setup_env.sh

--- backend/run.py
+++ backend/run.py
@@ -1,43 +1,4 @@
-import threading
-
-from app.config.paths import PICARX_CONFIG_FILE, PICARX_OLD_CONFIG_FILE
-from app.controllers.audio_controller import AudioController
-from app.controllers.camera_controller import CameraController
-from app.controllers.car_controller import CarController
-from app.controllers.files_controller import FilesController
-from app.endpoints.flask_setup import run_flask
-from app.util.file_util import copy_file_if_not_exists
-from app.util.logger import Logger
-
-logger = Logger(__name__)
-
-Logger.set_global_log_level(Logger.DEBUG)
-
-
-def run_app():
-    try:
-        copy_file_if_not_exists(PICARX_OLD_CONFIG_FILE, PICARX_CONFIG_FILE)
-        file_manager = FilesController()
-        audio_manager = AudioController()
-        camera_manager = CameraController()
-        car_manager = CarController(
-            camera_manager=camera_manager,
-            file_manager=file_manager,
-            audio_manager=audio_manager,
-        )
-        flask_thread = threading.Thread(
-            target=run_flask,
-            args=(car_manager, camera_manager, file_manager, audio_manager),
-        )
-        flask_thread.daemon = True
-        flask_thread.start()
-        car_manager.main()
-    except KeyboardInterrupt as e:
-        logger.info("Stopping application")
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-        logger.log_exception("Cancelled", e)
-
+from app.main import main
 
 if __name__ == "__main__":
-    run_app()
+    main()
--- backend/setup_env.sh
+++ backend/setup_env.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+echo "Installing portaudio19-dev, sox and libsox-fmt-mp3"
+sudo apt-get install portaudio19-dev sox libsox-fmt-mp3 -y
+echo "Creating venv"
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
+pip install -r requirements.txt
 
 # Check if the OS is Raspbian
 if ! grep -q "Raspbian" /etc/os-release; then
   echo "This is not a Raspbian OS. Setting up IDE environment..."
-  # Return to the scripts directory
-  echo "Installing dependencies."
-  pip install -r requirements.txt
   echo "Installing dev dependencies."
   pip install -r requirements-dev.txt
   echo "IDE environment setup completed."
