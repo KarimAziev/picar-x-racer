586ae8313461ff30d33713d91d564fd9551a5493
KarimAziev <karim.aziiev@gmail.com>
2024-09-13 09:54:38 +0300

    feat: Add retry logic for WebSocket connection in ImageFeed.vue

--- frontend/src/ui/ImageFeed.vue
+++ frontend/src/ui/ImageFeed.vue
@@ -1,5 +1,7 @@
 <template>
+  <ScanLines v-if="imgLoading || !connected" class="scan" />
   <img
+    v-else
     ref="imgRef"
     class="image-feed"
     :class="{
@@ -11,13 +13,17 @@
 
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from "vue";
+import ScanLines from "@/ui/ScanLines.vue";
 
 const ws = ref<WebSocket>();
 const WS_URL: string = `ws://${window.location.hostname}:${8050}`;
 const imgRef = ref();
 const imgLoading = ref(true);
+const connected = ref(false);
+const reconnectedEnabled = ref(true);
+const retryTimer = ref<NodeJS.Timeout | null>(null);
 
-onMounted(() => {
+const initWS = () => {
   ws.value = new WebSocket(WS_URL);
   if (!ws.value) {
     return;
@@ -25,6 +31,7 @@ onMounted(() => {
   ws.value.binaryType = "arraybuffer";
 
   ws.value.onmessage = (wsMsg: MessageEvent) => {
+    connected.value = true;
     imgLoading.value = false;
     const arrayBufferView = new Uint8Array(wsMsg.data);
     const blob = new Blob([arrayBufferView], { type: "image/jpeg" });
@@ -35,9 +42,29 @@ onMounted(() => {
 
   ws.value.onclose = (_: CloseEvent) => {
     console.log("closed");
+    connected.value = false;
+    imgLoading.value = true;
+    retryConnection();
   };
+};
+
+const retryConnection = () => {
+  if (retryTimer.value) {
+    clearTimeout(retryTimer.value);
+  }
+  if (reconnectedEnabled.value && !connected.value) {
+    retryTimer.value = setTimeout(() => {
+      console.log("Retrying WebSocket connection...");
+      initWS();
+    }, 5000);
+  }
+};
+
+onMounted(() => {
+  initWS();
 });
 onUnmounted(() => {
+  reconnectedEnabled.value = false;
   if (ws.value) {
     ws.value.close();
   }
@@ -61,5 +88,9 @@ onUnmounted(() => {
   height: 100%;
   box-shadow: 0px 0px 4px 2px var(--robo-color-primary);
   user-select: none;
+}
+.scan {
+  width: 100%;
+  height: 100%;
 }
 </style>
