8f49936c3a4c505300bb550a51c1bddc1a7217f3
KarimAziev <karim.aziiev@gmail.com>
2024-09-18 13:44:32 +0300

    feat: improve error messages

--- frontend/src/util/error.ts
+++ frontend/src/util/error.ts
@@ -1,6 +1,12 @@
 import { isAxiosError } from "axios";
 
-import { isError } from "@/util/guards";
+import {
+  isArray,
+  isError,
+  isNumber,
+  isPlainObject,
+  isString,
+} from "@/util/guards";
 
 export const retrieveResponseBody = (error: unknown) => {
   try {
@@ -10,11 +16,35 @@ export const retrieveResponseBody = (error: unknown) => {
   }
 };
 
+const stringifyError = (data: any): string => {
+  if (isString(data)) {
+    return data;
+  }
+  if (isNumber(data)) {
+    return `${data}`;
+  }
+  if (isArray(data)) {
+    return data.map(stringifyError).join("\n");
+  }
+  if (isPlainObject(data)) {
+    return Object.entries(data)
+      .map(([key, value]) => {
+        const str = `${key}: ${stringifyError(value)}`;
+        return str;
+      })
+      .join("\n");
+  }
+  return "";
+};
+
 export const retrieveError = <Err>(error: Err) => {
   if (isAxiosError(error)) {
+    const data = error.response?.data;
     return {
-      title: error.response?.statusText || "Error",
-      text: error.message,
+      title: error.response?.statusText
+        ? `${error.response?.statusText}: `
+        : "",
+      text: stringifyError(data?.error || data || error.message),
     };
   }
 
