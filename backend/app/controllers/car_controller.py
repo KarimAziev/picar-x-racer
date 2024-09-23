b5040b7413d3300b8dc80e287f14c8042aeb581c
KarimAziev <karim.aziiev@gmail.com>
2024-09-23 15:07:59 +0300

    fix: instantiate Picarx within CarController constructor

--- backend/app/controllers/car_controller.py
+++ backend/app/controllers/car_controller.py
@@ -13,9 +13,9 @@ from fastapi import WebSocket
 class CarController(metaclass=SingletonMeta):
     DEBOUNCE_INTERVAL = timedelta(seconds=1)
 
-    def __init__(self, picarx: Picarx):
+    def __init__(self):
         self.logger = Logger(name=__name__)
-        self.px = picarx
+        self.px = Picarx()
         self.avoid_obstacles_controller = AvoidObstaclesController(self.px)
         self.calibration = CalibrationController(self.px)
 
