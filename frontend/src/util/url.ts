87250b97e22675708284ee1f906cff2c3624ed7f
KarimAziev <karim.aziiev@gmail.com>
2024-09-27 19:34:52 +0300

    fix: use makeUrl for dynamic API endpoint generation

--- frontend/src/util/url.ts
+++ frontend/src/util/url.ts
@@ -9,3 +9,15 @@ export const makeWebsocketUrl = (path: string, port?: number) => {
 
   return baseUrl.toString();
 };
+
+export const makeUrl = (path: string, port?: number) => {
+  const protocol = window.location.protocol === "https:" ? "https:" : "http:";
+  const baseUrl = new URL(window.location.href);
+  baseUrl.protocol = protocol;
+  baseUrl.pathname = path;
+  if (port) {
+    baseUrl.port = `${port}`;
+  }
+
+  return baseUrl.toString();
+};
