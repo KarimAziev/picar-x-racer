b02b46c26b26a2c86a099c960a0fa775e3a5fb67
KarimAziev <karim.aziiev@gmail.com>
2024-10-03 19:54:29 +0300

    refactor: remove encode_and_detect function for simplification

--- backend/app/util/video_utils.py
+++ backend/app/util/video_utils.py
@@ -1,9 +1,8 @@
 from typing import Callable, Optional, Sequence
+
 import cv2
 import numpy as np
-
 from app.util.logger import Logger
-
 
 logger = Logger(__name__)
 
@@ -26,37 +25,14 @@ def encode(
     """
 
     if frame_enhancer:
-        frame_array = frame_enhancer(frame_array)
+        try:
+            frame_array = frame_enhancer(frame_array)
+        except Exception as err:
+            logger.log_exception(
+                f"Error in frame enhancer '{frame_enhancer.__name__}': ", err
+            )
 
     frame = frame_array
 
     _, buffer = cv2.imencode(format, frame, params)
-    return buffer.tobytes()
-
-
-def encode_and_detect(
-    frame_array: np.ndarray,
-    format=".jpg",
-    params: Sequence[int] = [],
-    frame_enhancer: Optional[Callable[[np.ndarray], np.ndarray]] = None,
-    detection_func: Optional[Callable[[np.ndarray], np.ndarray]] = None,
-):
-    """
-    Encode the frame array to the specified format.
-
-    Args:
-        frame_array (np.ndarray): Frame array to be encoded.
-        format (str): Encoding format (default is ".jpg").
-
-    Returns:
-        bytes: Encoded frame as a byte array.
-    """
-
-    if detection_func:
-        frame_array = detection_func(frame_array)
-
-    if frame_enhancer:
-        frame_array = frame_enhancer(frame_array)
-
-    _, buffer = cv2.imencode(format, frame_array, params)
     return buffer.tobytes()
