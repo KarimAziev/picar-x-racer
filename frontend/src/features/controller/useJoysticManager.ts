450de2944c6a1bb7a563730d7bff88b90a857e05
KarimAziev <karim.aziiev@gmail.com>
2024-10-13 18:44:21 +0300

    refactor: simplify joystick control logic

--- frontend/src/features/controller/useJoysticManager.ts
+++ frontend/src/features/controller/useJoysticManager.ts
@@ -19,16 +19,10 @@ export const useJoystickControl = (
         controllerStore.accelerate();
       } else if (direction > 135 && direction <= 225) {
         controllerStore.left();
-        if (controllerStore.speed === 0) {
-          controllerStore.accelerate();
-        }
       } else if (direction > 225 && direction <= 315) {
         controllerStore.decelerate();
       } else {
         controllerStore.right();
-        if (controllerStore.speed === 0) {
-          controllerStore.accelerate();
-        }
       }
     } else {
       controllerStore.slowdown();
