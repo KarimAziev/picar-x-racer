b52d9ab7dea3b828d47450501f7f3288e095a624
KarimAziev <karim.aziiev@gmail.com>
2024-10-31 19:36:03 +0200

    feat: enhance mobile detection with extended regex pattern

--- frontend/src/composables/useDeviceWatcher.ts
+++ frontend/src/composables/useDeviceWatcher.ts
@@ -1,10 +1,12 @@
 import { ref, onBeforeUnmount, onMounted } from "vue";
 
 export const useDeviceWatcher = () => {
-  const isMobile = ref(/Mobi|Android|iPhone/i.test(navigator.userAgent));
+  const re =
+    /Mobi|Android|iPhone|SAMSUNG|SGH-[I|N|T]|GT-[I|P|N]|SM-[N|P|T|Z|G]|SHV-E|SCH-[I|J|R|S]|SPH-L/i;
+  const isMobile = ref(re.test(navigator.userAgent));
 
   const updateDeviceStatus = () => {
-    isMobile.value = /Mobi|Android|iPhone/i.test(navigator.userAgent);
+    isMobile.value = re.test(navigator.userAgent);
   };
 
   onMounted(() => {
