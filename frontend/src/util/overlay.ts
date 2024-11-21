d23d76ba9254b138c43cb4a5ae281b2de5386c7e
KarimAziev <karim.aziiev@gmail.com>
2024-11-21 10:40:08 +0200

    refactor: modularize overlay drawing functions

--- frontend/src/util/overlay.ts
+++ frontend/src/util/overlay.ts
@@ -1,16 +1,76 @@
 import { DetectionResult } from "@/features/controller/detectionStore";
 
-export const drawOverlay = (
-  canvas: HTMLCanvasElement,
-  img: HTMLImageElement,
-  detectionData?: DetectionResult[],
+export const drawLabelWithConfidence = (
+  label: string,
+  confidence: number,
+  ctx: CanvasRenderingContext2D,
+  x1: number,
+  y1: number,
 ) => {
-  if (!canvas || !img) return;
+  ctx.fillText(
+    `${label.toUpperCase()}: ${(confidence * 100).toFixed(1)}%`,
+    x1,
+    y1 - 10,
+  );
+};
+
+export const drawDetectionOverlay = (
+  ctx: CanvasRenderingContext2D,
+  scaleX: number,
+  scaleY: number,
+  detection: DetectionResult,
+) => {
+  const { label, confidence, bbox } = detection;
+  let [x1, y1, x2, y2] = bbox;
+
+  x1 = x1 * scaleX;
+  y1 = y1 * scaleY;
+  x2 = x2 * scaleX;
+  y2 = y2 * scaleY;
+
+  ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+
+  drawLabelWithConfidence(label, confidence, ctx, x1, y1);
+};
+
+export const drawDetectionCrosshair = (
+  ctx: CanvasRenderingContext2D,
+  scaleX: number,
+  scaleY: number,
+  detection: DetectionResult,
+) => {
+  const { label, confidence, bbox } = detection;
+  let [x1, y1, x2, y2] = bbox;
+
+  x1 = x1 * scaleX;
+  y1 = y1 * scaleY;
+  x2 = x2 * scaleX;
+  y2 = y2 * scaleY;
+
+  const midX = (x1 + x2) / 2;
+  const midY = (y1 + y2) / 2;
+
+  ctx.beginPath();
+  ctx.moveTo(x1, midY);
+  ctx.lineTo(x2, midY);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(midX, y1);
+  ctx.lineTo(midX, y2);
+  ctx.stroke();
+  drawLabelWithConfidence(label, confidence, ctx, x1, y1);
+};
+
+export const setupCtx = (canvas: HTMLCanvasElement, img: HTMLImageElement) => {
+  if (!canvas || !img) {
+    return {};
+  }
 
   const ctx = canvas!.getContext("2d");
 
   if (!ctx) {
-    return;
+    return {};
   }
 
   const originalWidth = img.naturalWidth;
@@ -36,23 +96,40 @@ export const drawOverlay = (
   ctx.strokeStyle = color;
   ctx.fillStyle = color;
 
+  return {
+    ctx,
+    scaleX,
+    scaleY,
+  };
+};
+
+export const drawDetectionsWith = (
+  fn: (
+    ctx: CanvasRenderingContext2D,
+    scaleX: number,
+    scaleY: number,
+    detection: DetectionResult,
+  ) => void,
+  canvas: HTMLCanvasElement,
+  img: HTMLImageElement,
+  detectionData?: DetectionResult[],
+) => {
+  const { ctx, scaleX, scaleY } = setupCtx(canvas, img);
+  if (!ctx) {
+    return;
+  }
+
   detectionData?.forEach((detection) => {
-    const { label, confidence, bbox } = detection;
-    let [x1, y1, x2, y2] = bbox;
-
-    x1 = x1 * scaleX;
-    y1 = y1 * scaleY;
-    x2 = x2 * scaleX;
-    y2 = y2 * scaleY;
-
-    ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
-
-    ctx.fillText(
-      `${label.toUpperCase()}: ${(confidence * 100).toFixed(1)}%`,
-      x1 + 5,
-      y1 + 25,
-    );
+    fn(ctx, scaleX, scaleY, detection);
   });
+};
+
+export const drawOverlay = (
+  canvas: HTMLCanvasElement,
+  img: HTMLImageElement,
+  detectionData?: DetectionResult[],
+) => {
+  return drawDetectionsWith(drawDetectionOverlay, canvas, img, detectionData);
 };
 
 export const drawAimOverlay = (
@@ -60,68 +137,5 @@ export const drawAimOverlay = (
   img: HTMLImageElement,
   detectionData?: DetectionResult[],
 ) => {
-  if (!canvas || !img) return;
-
-  const ctx = canvas.getContext("2d");
-
-  if (!ctx) {
-    return;
-  }
-
-  const originalWidth = img.naturalWidth;
-  const originalHeight = img.naturalHeight;
-
-  const displayedWidth = img.clientWidth;
-  const displayedHeight = img.clientHeight;
-
-  const scaleX = displayedWidth / originalWidth;
-  const scaleY = displayedHeight / originalHeight;
-
-  canvas.width = displayedWidth;
-  canvas.height = displayedHeight;
-
-  // Clear the canvas before drawing new frame
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  const styles = getComputedStyle(document.documentElement);
-  const color = styles.getPropertyValue("--color-text").trim();
-  const font = styles.getPropertyValue("--canvas-font").trim();
-
-  ctx.lineWidth = 4;
-  ctx.font = font;
-  ctx.strokeStyle = color; // Set the crosshair color
-  ctx.fillStyle = color; // Set the text color
-
-  detectionData?.forEach((detection) => {
-    const { label, confidence, bbox } = detection;
-    let [x1, y1, x2, y2] = bbox;
-
-    // Adjust bounding box coordinates to the canvas scale
-    x1 = x1 * scaleX;
-    y1 = y1 * scaleY;
-    x2 = x2 * scaleX;
-    y2 = y2 * scaleY;
-
-    const midX = (x1 + x2) / 2; // Mid-point of the bounding box (horizontal)
-    const midY = (y1 + y2) / 2; // Mid-point of the bounding box (vertical)
-
-    // Full-width horizontal line across the bounding box
-    ctx.beginPath();
-    ctx.moveTo(x1, midY); // Start at the left edge of the bbox
-    ctx.lineTo(x2, midY); // Draw to the right edge of the bbox
-    ctx.stroke();
-
-    // Full-height vertical line through the bounding box
-    ctx.beginPath();
-    ctx.moveTo(midX, y1); // Start at the top edge of the bbox
-    ctx.lineTo(midX, y2); // Draw to the bottom edge of the bbox
-    ctx.stroke();
-
-    // Draw the label with detection confidence slightly above the bbox
-    ctx.fillText(
-      `${label.toUpperCase()}: ${(confidence * 100).toFixed(1)}%`,
-      x1, // Start slightly to the left of the bbox
-      y1 - 10, // Position it slightly above the top of the bbox
-    );
-  });
+  return drawDetectionsWith(drawDetectionCrosshair, canvas, img, detectionData);
 };
