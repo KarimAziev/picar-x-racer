3852b3617594861431b4bfc9e1d58633054bb0de
KarimAziev <karim.aziiev@gmail.com>
2024-11-23 10:47:12 +0200

    feat: add pick function to obj utility module

--- frontend/src/util/obj.ts
+++ frontend/src/util/obj.ts
@@ -19,3 +19,16 @@ export const pickAll = <V extends Record<string, any>, K extends keyof V>(
     return acc;
   }, {} as V);
 };
+
+export const pick = <V extends Record<string, any>, K extends keyof V>(
+  props: K[],
+  data: V,
+): Pick<V, K> => {
+  return props.reduce((acc, key) => {
+    if (key in data) {
+      acc[key as K] = data[key];
+    }
+
+    return acc;
+  }, {} as V);
+};
