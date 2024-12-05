b7d3c869c013c4204a60a83ca4177b38c42c2c30
KarimAziev <karim.aziiev@gmail.com>
2024-12-05 15:11:33 +0200

    feat: Add max speed control to GeneralPanel component

--- frontend/src/features/settings/components/GeneralPanel.vue
+++ frontend/src/features/settings/components/GeneralPanel.vue
@@ -9,6 +9,16 @@
       :step="50"
       showButtons
     />
+    <NumberField
+      label="The Maximum Speed"
+      v-model="store.data.max_speed"
+      inputId="settings.data.max_speed"
+      :min="10"
+      :step="10"
+      :max="100"
+      showButtons
+      @update:model-value="handleUpdateMaxSpeed"
+    />
     <VideoSettings />
   </Panel>
 
@@ -23,6 +33,14 @@ import SwitchSettings from "@/features/settings/components/SwitchSettings.vue";
 import VideoSettings from "@/features/settings/components/VideoSettings.vue";
 import Panel from "@/ui/Panel.vue";
 import NumberField from "@/ui/NumberField.vue";
+import { useControllerStore } from "@/features/controller/store";
 
 const store = useSettingsStore();
+const controllerStore = useControllerStore();
+
+const handleUpdateMaxSpeed = (value: number) => {
+  if (controllerStore.model?.connected) {
+    controllerStore.setMaxSpeed(value);
+  }
+};
 </script>
