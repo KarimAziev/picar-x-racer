d38b81136db140cbcb77f131a763b74d653f40ee
KarimAziev <karim.aziiev@gmail.com>
2024-12-15 19:27:38 +0200

    fix: add missing PhotoButton component for capturing photos

---
+++ frontend/src/ui/PhotoButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <Button
+    class="opacity-hover"
+    @click="controllerStore.takePhoto"
+    icon="pi pi-camera"
+    text
+    aria-label="capture-photo"
+    v-tooltip="'Capture a photo'"
+  />
+</template>
+
+<script setup lang="ts">
+import { useControllerStore } from "@/features/controller/store";
+
+defineProps<{ class?: string }>();
+
+const controllerStore = useControllerStore();
+</script>
