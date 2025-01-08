03e730821b717e8540ef2111f5de0345659a0acc
KarimAziev <karim.aziiev@gmail.com>
2025-01-08 22:13:38 +0200

    feat: handle fullscreen

--- frontend/src/composables/useWindowSize.ts
+++ frontend/src/composables/useWindowSize.ts
@@ -5,12 +5,16 @@ export const useWindowSize = () => {
   const isMobile = useDeviceWatcher();
   const getHeight = () =>
     isMobile.value
-      ? Math.min(window.screen.availHeight, window.innerHeight)
+      ? document.fullscreenElement
+        ? Math.max(window.screen.availHeight, window.innerHeight)
+        : Math.min(window.screen.availHeight, window.innerHeight)
       : window.innerHeight;
 
   const getWidth = () =>
     isMobile.value
-      ? Math.min(window.screen.availWidth, window.innerWidth)
+      ? document.fullscreenElement
+        ? Math.max(window.screen.availWidth, window.innerWidth)
+        : Math.min(window.screen.availWidth, window.innerWidth)
       : window.innerWidth;
 
   const width = ref(getWidth());
@@ -22,10 +26,12 @@ export const useWindowSize = () => {
 
   const addResizeListeners = () => {
     window.addEventListener("resize", handleResize);
+    window.addEventListener("fullscreenchange", handleResize);
   };
 
   const removeResizeListeners = () => {
     window.removeEventListener("resize", handleResize);
+    window.removeEventListener("fullscreenchange", handleResize);
   };
 
   onMounted(() => {
