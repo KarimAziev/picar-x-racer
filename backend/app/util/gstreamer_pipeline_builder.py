dfa0018747b8173d684bbd1593734d8f083866df
KarimAziev <karim.aziiev@gmail.com>
2025-01-25 19:59:42 +0200

    refactor: enhance NV12 pipeline with x264 encoding options

--- backend/app/util/gstreamer_pipeline_builder.py
+++ backend/app/util/gstreamer_pipeline_builder.py
@@ -25,7 +25,8 @@ class GstreamerPipelineBuilder:
         "UYVY": ("video/x-raw, format=UYVY", "videoconvert"),  # YUV variant -> Convert
         "NV12": (
             "video/x-raw, format=NV12",
-            "h264parse ! v4l2h264dec",
+            "x264enc tune=zerolatency bitrate=4000 ! speed-preset=ultrafast ! \
+      h264parse ! v4l2h264dec | videoconvert",
         ),  # YUV 4:2:0 -> Convert
         "YV12": ("video/x-raw, format=YV12", "videoconvert"),  # YUV variant -> Convert
         "NV21": ("video/x-raw, format=NV21", "videoconvert"),  # NV21 variant -> Convert
