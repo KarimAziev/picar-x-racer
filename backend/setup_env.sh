369b10ab0a7aaab8a45aa390066f5675a5d5c2a8
KarimAziev <karim.aziiev@gmail.com>
2025-02-02 14:53:00 +0200

    build: add compilation of OpenCV with gstreamer

--- backend/setup_env.sh
+++ backend/setup_env.sh
@@ -85,7 +85,7 @@ if [ -d ".venv" ]; then
   log_info "Virtual environment '.venv' already exists. Skipping creation."
 else
   log_info "Creating Python virtual environment in '.venv'..."
-  python3 -m venv .venv
+  python3 -m venv --system-site-packages .venv
 fi
 
 log_info "Activating Python virtual environment..."
@@ -128,29 +128,45 @@ install_dbus_python_dependencies() {
 }
 
 compile_cv2() {
+  if [ "$(id -u)" -ne 0 ]; then
+    SUDO="sudo"
+  else
+    SUDO=""
+  fi
+
   pip uninstall opencv-python -y
+
+  log_info "Installing open-cv2 dependencies..."
 
   # Install essential dependencies for compiling OpenCV with desired features
   # These include tools for building software (build-essential, cmake, python3-dev)
   # and libraries for multimedia (GStreamer, FFmpeg, libav), image I/O (libpng, libjpeg, libtiff, etc.),
   # GUI functionality (libgtk-3-dev), and Python bindings (python3-numpy).
-  sudo apt install build-essential cmake git python3-dev python3-numpy \
+  $SUDO apt install build-essential cmake git python3-dev python3-numpy \
     libavcodec-dev libavformat-dev libswscale-dev \
     libgstreamer-plugins-base1.0-dev \
     libgstreamer1.0-dev libgtk-3-dev \
     libpng-dev libjpeg-dev libopenexr-dev libtiff-dev libwebp-dev \
     libopencv-dev x264 libx264-dev libssl-dev ffmpeg
 
+  # we need this for:
+  # python3 -c 'import gi; gi.require_version("Gst", "1.0"); from gi.repository import Gst; Gst.init(None); print("GStreamer version:", Gst.version())'
+  $SUDO apt-get install python3-gi gir1.2-gstreamer-1.0
+
+  log_info "Compiling open-cv2. Note, this may take a while..."
+
   # Reinstall OpenCV from source, instead of using pre-built binaries
   # The `--no-binary opencv-python` forcibly disables downloading pre-built packages from PyPI
   # and forces compiling OpenCV from source with system-level dependencies.
   # This ensures GStreamer and other optional functionalities will be included if their dependencies are available.
   python -m pip install --no-binary opencv-python opencv-python
+  log_info "open-cv2 is installed and compiled."
 }
 
 if is_raspberry_pi; then
   log_info "Raspberry Pi detected. Installing system-level dependencies for 'dbus-python'..."
   install_dbus_python_dependencies
+  compile_cv2
   log_info "Raspberry Pi detected. Standard installation complete."
 else
   log_info "Non-Raspberry Pi system detected. Setting up IDE environment..."
@@ -160,6 +176,7 @@ else
   fi
   log_info "Installing development dependencies from requirements-dev.txt..."
   pip install -r requirements-dev.txt
+  compile_cv2
   log_info "IDE environment setup completed."
 fi
 
