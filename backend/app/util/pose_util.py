fix: correct grid shape calculation in pose_util.py

backend/app/util/pose_util.py
@@ -156,7 +156,7 @@ def _yolov8_decoding(
         raw_boxes, raw_kpts, strides, np.arange(3)
     ):
         # create grid
-        shape = [int(x / stride) for x in image_dims]
+        shape = box_distribute.shape[1:3]
         grid_x = np.arange(shape[1]) + 0.5
         grid_y = np.arange(shape[0]) + 0.5
         grid_x, grid_y = np.meshgrid(grid_x, grid_y)